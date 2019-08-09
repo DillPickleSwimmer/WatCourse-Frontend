d5d96e5c01b94f30b368a7e566d256ab12e2c9f9
DillPickleSwimmer <dillpickleswimmer@GMAIL.COM>
2019-08-08 22:15:35 -0400

    pretty changes

--- watcourse/src/components/Footer.js
+++ watcourse/src/components/Footer.js
@@ -15,12 +15,12 @@ class Footer extends React.Component {
     defaultState = {
         openForm: null,
         emotion: -1,
-        bugMsg: "",
-        screenshot: "", 
+        bugMsg: '',
+        screenshot: '', 
         bestFeature: [],
-        bestFeatureMsg: "", 
-        challenges: "",
-        futureFeature: "",
+        bestFeatureMsg: '', 
+        challenges: '',
+        futureFeature: '',
     };
 
     constructor(props) {
@@ -36,16 +36,16 @@ class Footer extends React.Component {
     }
 
     emotionSelect(id) {
-        document.getElementById("forms").style.height = "auto";
+        document.getElementById('forms').style.maxHeight = '700px';
         this.setState({
-            openForm: "FEEDBACK",
+            openForm: 'FEEDBACK',
             emotion: id,
         })
     }
 
     openBugForm() {
-        document.getElementById("forms").style.height = "auto";
-        this.setState({openForm: "BUG"});
+        document.getElementById('forms').style.maxHeight = '700px';
+        this.setState({openForm: 'BUG'});
     }
 
     closeForm() {
@@ -54,13 +54,13 @@ class Footer extends React.Component {
             // TODO: submit feedback
         }
 
-        document.getElementById("forms").style.height = "0px";
+        document.getElementById('forms').style.maxHeight = '0px';
         this.setState(this.defaultState);
     }
 
     updateField(field, event) {
         var newState = {};
-        if( field === "bestFeature" ) {
+        if( field === 'bestFeature' ) {
             var options = event.target.options;
             var value = [];
             for (var i = 0, l = options.length; i < l; i++) {
@@ -79,92 +79,92 @@ class Footer extends React.Component {
         console.log(this.state);
 
         switch(this.state.openForm) {
-            case "FEEDBACK":
+            case 'FEEDBACK':
                 // TODO: submit feedback
                 break;
-            case "BUG":
+            case 'BUG':
                 // TODO: submit bug
                 break;
             default: 
                 return null;
         }
-        this.setState(this.defaultState);
+        this.closeForm();
     }
 
     render() {
         const feedbackForm = (
-            <div className="form">
-                <div className="title">Provide Feedback</div>
+            <div className='form'>
+                <div className='title'>Provide Feedback</div>
                 <div>What features do you find most helpful? (hold ctrl to multi-select)<br/>
-                    <select multiple value={this.state.bestFeature} onChange={this.updateField.bind(this, "bestFeature")}>
-                        <option value="term-planning">Term Planning</option>
-                        <option value="req-validation">Requirement Validation</option>
-                        <option value="prereq-tree">Pre-requisite Tree</option>
-                        <option value="transcript">Transcript Parsing</option>
-                        <option value="auto-plan">Auto-planning (in-progress)</option>
+                    <select multiple value={this.state.bestFeature} onChange={this.updateField.bind(this, 'bestFeature')}>
+                        <option value='term-planning'>Term Planning</option>
+                        <option value='req-validation'>Requirement Validation</option>
+                        <option value='prereq-tree'>Pre-requisite Tree</option>
+                        <option value='transcript'>Transcript Parsing</option>
+                        <option value='auto-plan'>Auto-planning (in-progress)</option>
                     </select>
-                    <br /><br />
-                    <textarea value={this.state.bestFeatureMsg} onChange={this.updateField.bind(this, "bestFeatureMsg")}/>
+                </div>
+                <div>What do you like about the app so far? <br/>
+                    <textarea value={this.state.bestFeatureMsg} onChange={this.updateField.bind(this, 'bestFeatureMsg')}/>
                 </div>
                 <div>What challenges do you find using the app? <br/>
-                    <textarea value={this.state.challenges} onChange={this.updateField.bind(this, "challenges")}/>
+                    <textarea value={this.state.challenges} onChange={this.updateField.bind(this, 'challenges')}/>
                 </div>
                 <div>What features would you like to see? <br/> 
-                    <textarea value={this.state.futureFeature} onChange={this.updateField.bind(this, "futureFeature")}/>
+                    <textarea value={this.state.futureFeature} onChange={this.updateField.bind(this, 'futureFeature')}/>
                 </div>
+                <button onClick={this.submitForm}>Submit</button>
             </div>
         );
 
         const bugForm = (
-            <div className="form">
-                <div className="title">Report a Bug</div>
-                <div>Description: <textarea value={this.state.bugMsg} onChange={this.updateField.bind(this, "bugMsg")}/></div>
+            <div className='form'>
+                <div className='title'>Report a Bug</div>
+                <div>Description: <textarea value={this.state.bugMsg} onChange={this.updateField.bind(this, 'bugMsg')}/></div>
                 <div>Screenshot (optional): 
-                    <input type="file" value={this.state.screenshot} onChange={this.updateField.bind(this, "screenshot")}/>
+                    <input type='file' value={this.state.screenshot} onChange={this.updateField.bind(this, 'screenshot')}/>
                 </div>
+                <button onClick={this.submitForm}>Submit</button>
             </div>
         );
 
         return (
-            <div className="Footer">
-                <div className="forms" id="forms">
-                    <div className="form">
-                        {
-                            function(openForm) {
-                                switch(openForm) {
-                                    case "FEEDBACK":
-                                        return feedbackForm;
-                                    case "BUG":
-                                        return bugForm;
-                                    default: 
-                                        return null;
-                                }
-                            }(this.state.openForm)
-                        }
-                        <button onClick={this.submitForm}>Submit</button>
-                    </div>
-                    <CloseIcon className="small-icon" onClick={this.closeForm} /> 
+            <div className='Footer'>
+                <div className='forms' id='forms'>
+                    {
+                        function(openForm) {
+                            switch(openForm) {
+                                case 'FEEDBACK':
+                                    return feedbackForm;
+                                case 'BUG':
+                                    return bugForm;
+                                default: 
+                                    return null;
+                            }
+                        }(this.state.openForm)
+                    }
+                    <CloseIcon className='small-icon' onClick={this.closeForm} /> 
                 </div>
-                <div className="inner-footer">
+                <div className='inner-footer'>
                     <div></div>
-                    <div className="links">
-                        <a href="https://github.com/DillPickleSwimmer/WatCourse-Frontend">Frontend Github</a>
-                        <a href="https://github.com/SiddharthVaknalli/WatCourse-backend">Backend Github</a>
+                    <div className='links'>
+                        <a href='https://github.com/DillPickleSwimmer/WatCourse-Frontend'>Frontend Github</a>
+                        <a href='https://github.com/SiddharthVaknalli/WatCourse-backend'>Backend Github</a>
                     </div>
-                    <div className="feedback">
-                        <div className="feedback-emotions">
-                            <Emotion0 className={`icon ${this.state.emotion === 0 ? "selected-emotion" : null}`} 
+                    <div className='feedback'>
+                        <div className='feedback-emotions'>
+                            <Emotion0 className={`icon ${this.state.emotion === 0 ? 'selected-emotion' : null}`} 
                                 onClick={this.emotionSelect.bind(this, 0)} />
-                            <Emotion1 className={`icon ${this.state.emotion === 1 ? "selected-emotion" : null}`} 
+                            <Emotion1 className={`icon ${this.state.emotion === 1 ? 'selected-emotion' : null}`} 
                                 onClick={this.emotionSelect.bind(this, 1)} />
-                            <Emotion2 className={`icon ${this.state.emotion === 2 ? "selected-emotion" : null}`} 
+                            <Emotion2 className={`icon ${this.state.emotion === 2 ? 'selected-emotion' : null}`} 
                                 onClick={this.emotionSelect.bind(this, 2)} />
-                            <Emotion3 className={`icon ${this.state.emotion === 3 ? "selected-emotion" : null}`} 
+                            <Emotion3 className={`icon ${this.state.emotion === 3 ? 'selected-emotion' : null}`} 
                                 onClick={this.emotionSelect.bind(this, 3)} />
-                            <Emotion4 className={`icon ${this.state.emotion === 4 ? "selected-emotion" : null}`} 
+                            <Emotion4 className={`icon ${this.state.emotion === 4 ? 'selected-emotion' : null}`} 
                                 onClick={this.emotionSelect.bind(this, 4)} />
                         </div>
-                        <BugIcon className="icon" onClick={this.openBugForm} />
+                        <BugIcon className='icon' onClick={this.openBugForm} />
                     </div>
                 </div>
             </div>
