1a0274ee4302b685a40025e5cdb4aa6f7fed09ed
DillPickleSwimmer <dillpickleswimmer@GMAIL.COM>
2019-08-09 00:03:10 -0400

    support submittting feedback

--- watcourse/src/components/Footer.js
+++ watcourse/src/components/Footer.js
@@ -1,6 +1,9 @@
 import  React from 'react';
+import { connect } from 'react-redux';
 
 import '../styles/Footer.css';
+
+import { reportBug, submitFeedback } from '../actions/feedbackActions';
 
 import { ReactComponent as Emotion0 } from '../images/icon_emotion_0.svg';
 import { ReactComponent as Emotion1 } from '../images/icon_emotion_1.svg';
@@ -10,13 +13,14 @@ import { ReactComponent as Emotion4 } from '../images/icon_emotion_4.svg';
 //import { ReactComponent as FeedbackIcon } from '../images/icon_feedback.svg';
 import { ReactComponent as BugIcon } from '../images/icon_bug.svg';
 import { ReactComponent as CloseIcon } from '../images/icon_close.svg';
+import { toggleSidebarSearch } from '../actions/sidebarActions';
 
 class Footer extends React.Component {
     defaultState = {
         openForm: null,
         emotion: -1,
         bugMsg: '',
-        screenshot: '', 
+        screenshot: [], 
         bestFeature: [],
         bestFeatureMsg: '', 
         challenges: '',
@@ -26,11 +30,11 @@ class Footer extends React.Component {
     constructor(props) {
         super(props); 
 
-        //this.emotionSelect = this.emotionSelect.bind(this);
         this.openBugForm = this.openBugForm.bind(this);
         this.closeForm = this.closeForm.bind(this);
         this.submitForm = this.submitForm.bind(this);
-        //this.updateField = this.updateField.bind(this);
+
+        this.screenshots = React.createRef();
 
         this.state = this.defaultState;
     }
@@ -75,18 +79,23 @@ class Footer extends React.Component {
         this.setState(newState);
     }
 
-    submitForm() {
-        console.log(this.state);
-
+    submitForm(event) {
+        event.preventDefault();
         switch(this.state.openForm) {
             case 'FEEDBACK':
-                // TODO: submit feedback
+                this.props.dispatch(submitFeedback(
+                    this.state.emotion, 
+                    `${this.state.bestFeature} - ${this.state.bestFeatureMsg}`,
+                    this.state.challenges,
+                    this.state.features,
+                ))
                 break;
             case 'BUG':
-                // TODO: submit bug
+                //screenshots
+                let files = this.screenshots.current.files || [];
+                this.props.dispatch(reportBug(this.state.bugMsg, files));
                 break;
-            default: 
-                return null;
+            default: return null;
         }
         this.closeForm();
     }
@@ -113,7 +122,7 @@ class Footer extends React.Component {
                 <div>What features would you like to see? <br/> 
                     <textarea value={this.state.futureFeature} onChange={this.updateField.bind(this, 'futureFeature')}/>
                 </div>
-                <button onClick={this.submitForm}>Submit</button>
+                <button type="submit" onClick={this.submitForm}>Submit</button>
             </div>
         );
 
@@ -122,7 +131,14 @@ class Footer extends React.Component {
                 <div className='title'>Report a Bug</div>
                 <div>Description: <textarea value={this.state.bugMsg} onChange={this.updateField.bind(this, 'bugMsg')}/></div>
                 <div>Screenshot (optional): 
-                    <input type='file' value={this.state.screenshot} onChange={this.updateField.bind(this, 'screenshot')}/>
+                    <input 
+                        type='file' 
+                        value={this.state.screenshot} 
+                        onChange={this.updateField.bind(this, 'screenshot')}
+                        ref={this.screenshots}
+                        multiple
+                        required
+                    />
                 </div>
                 <button onClick={this.submitForm}>Submit</button>
             </div>
@@ -172,4 +188,4 @@ class Footer extends React.Component {
     }
 }
 
-export default Footer;
+export default connect(state=>({}))(Footer);
